ef49e3d76f274e1c6a8b419011764b6ac182ad3a
MarcDussault <marc.dussault-matte.1@ens.etsmtl.ca>
2017-03-27 20:13:27 -0400

    fenetre modifier

--- src/Views/Fenetre2.java
+++ src/Views/Fenetre2.java
@@ -8,17 +8,22 @@ import java.awt.EventQueue;
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JSplitPane;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -30,17 +35,17 @@ public class Fenetre2 extends JFrame {
 
 	
 
-	void insererImage() throws IOException 
+	void insererImageVignette() throws IOException 
 	{
 		
-			BufferedImage myPicture = ImageIO.read(new File("Warriors.jpg"));
-			Image myPictureResized = myPicture.getScaledInstance(panelVignette.getWidth() - 30, panelVignette.getHeight() - 30, Image.SCALE_SMOOTH );
-			ImageIcon imageIcon = new ImageIcon(myPictureResized);
+		BufferedImage myPicture = ImageIO.read(new File("Warriors.jpg"));
+		Image myPictureResized = myPicture.getScaledInstance(panelVignette.getWidth() - 30, panelVignette.getHeight() - 30, Image.SCALE_SMOOTH );
+		ImageIcon imageIcon = new ImageIcon(myPictureResized);
 			
-			JLabel picLabel = new JLabel(imageIcon);
+		JLabel picLabel = new JLabel(imageIcon);
 			
-			//Image scaledImage = originalImage.getScaledInstance(jPanel.getWidth(),jPanel.getHeight(),Image.SCALE_SMOOTH);
-			panelVignette.add(picLabel);			
+		//Image scaledImage = originalImage.getScaledInstance(jPanel.getWidth(),jPanel.getHeight(),Image.SCALE_SMOOTH);
+		panelVignette.add(picLabel);			
 	}
 	
 	
@@ -66,6 +71,11 @@ public class Fenetre2 extends JFrame {
 	 */
 	public Fenetre2() throws IOException {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JFileChooser selecteurFichier = new JFileChooser();
+		selecteurFichier.setCurrentDirectory(new File(System.getProperty("user.home")));
+		FileNameExtensionFilter filtreJPG = new FileNameExtensionFilter("jpg image (*.jpg)","jpg");
+
 		setBounds(100, 100, 1000, 600);
 		
 		JMenuBar menuBar = new JMenuBar();
@@ -83,8 +93,42 @@ public class Fenetre2 extends JFrame {
 		JMenu mnEdition = new JMenu("Edition");
 		menuBar.add(mnEdition);
 		
-		JMenuItem mntmInsrerImage = new JMenuItem("Ins\u00E9rer image");
-		mnEdition.add(mntmInsrerImage);
+		JMenuItem mntmInsererImage = new JMenuItem("Ins\u00E9rer image");
+		mnEdition.add(mntmInsererImage);
+		
+		mntmInsererImage.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				selecteurFichier.setFileFilter(filtreJPG);
+				int fichierChoisi = selecteurFichier.showOpenDialog(getParent());
+				
+				if (fichierChoisi == selecteurFichier.APPROVE_OPTION)
+				{
+					File fichierSelectionner = selecteurFichier.getSelectedFile();
+					String strFichier = fichierSelectionner.getName();
+					String extFichier = strFichier.substring(strFichier.lastIndexOf("."));
+					if (extFichier.equals(".jpg"))
+					{
+						try 
+						{
+							insererImageVignette();
+							System.out.println("inserer");
+						} 
+						catch (IOException e1)
+						{
+							e1.printStackTrace();
+						}
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(null, "Le fichier selectionné doit être en .jpg", "Erreur", JOptionPane.ERROR_MESSAGE);
+					}	
+				}
+			}
+
+	
+		});
+
 		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -104,27 +148,27 @@ public class Fenetre2 extends JFrame {
 		
 		panelVignette = new JPanel();
 		
-		panelVignette.setSize(new Dimension(150,150));
+		panelVignette.setSize(new Dimension(160,160));
 		panelVignette.setBorder(BorderFactory.createTitledBorder("Vignette"));
         //panelVignette.setBackground(Color.RED);
         panelVignette.setName("Vignette");
         
   		JPanel panelOutils = new JPanel();
-		panelOutils.setPreferredSize(new Dimension(60,120));
+		panelOutils.setPreferredSize(new Dimension(60,160));
 		panelOutils.setBorder(BorderFactory.createTitledBorder("Outils"));
         //panelOutils.setBackground(Color.GREEN);
         panelOutils.setName("Outils");
         
         
         JPanel panelTranslation = new JPanel();
-        panelTranslation.setPreferredSize(new Dimension(150,150));
+        panelTranslation.setPreferredSize(new Dimension(140,140));
         panelTranslation.setBorder(BorderFactory.createTitledBorder("Translation"));
         //panelTranslation.setBackground(Color.YELLOW);
         panelTranslation.setName("Translation");
         panelOutils.add(panelTranslation, BorderLayout.NORTH);
         
         JPanel panelZoom = new JPanel();
-        panelZoom.setPreferredSize(new Dimension(150,150));
+        panelZoom.setPreferredSize(new Dimension(140,140));
         panelZoom.setBorder(BorderFactory.createTitledBorder("Zoom"));
         //panelZoom.setBackground(Color.CYAN);
         panelZoom.setName("ZOOM");
@@ -133,7 +177,7 @@ public class Fenetre2 extends JFrame {
         panelGauche.add(panelVignette, BorderLayout.NORTH);
         panelGauche.add(panelOutils, BorderLayout.CENTER);
         
-        insererImage();
+        insererImageVignette();
 		
 	}
 	
