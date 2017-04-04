d0be4ae1a70bd6a1cb57b5bcf4f121281fe5a946
horty <hshlg1@yahoo.fr>
2017-04-04 05:16:05 -0700

    add Controller

--- src/Controller/ControllerMenu.java
+++ src/Controller/ControllerMenu.java
@@ -2,27 +2,19 @@ package Controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import Modeles.*;
 import Views.*;
 
-public class ControllerOutils implements ActionListener{
+public class ControllerMenu implements ActionListener{
 
-	private Modeles theModel;
+	private Modeles theModel;	
+	//private VueZoneDessin  vueZoneDessin;
 	
-	private VueOutils outils;
-	
-	
-	
-	public ControllerOutils(Modeles model, VueOutils vueOutils)
-	{
-		this.theModel = model;
-		this.outils = outils;
-	}
-
-
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
+
 }
